1df2454f2400b2840baf2a2d5d6ac2fb37edd73b
numi <numi1105@naver.com>
2025-02-10 17:41:50 +0900

    반응형디자인0210

--- js/script.js
+++ js/script.js
@@ -6,11 +6,7 @@ const visualSlider = new Swiper(".visual-slider", {
     },
     speed: 1000,
 
-    // Navigation arrows
-    navigation: {
-        nextEl: ".visual-slider .btn-next",
-        prevEl: ".visual-slider .btn-prev",
-    },
+    // navigation: false,
 
     pagination: {
         el: ".visual-slider .swiper-pagination",
@@ -19,7 +15,11 @@ const visualSlider = new Swiper(".visual-slider", {
 
     breakpoints: {
         800: {
-            navigation: false,
+            // Navigation arrows
+            navigation: {
+                nextEl: ".visual-slider .btn-next",
+                prevEl: ".visual-slider .btn-prev",
+            },
         },
     },
 });
@@ -57,11 +57,9 @@ const newList = new Swiper(".new-list", {
 });
 function initSwipers() {
     if (window.innerWidth > 800) {
-        visualSlider.init();
         bestList.init();
         newList.init();
     } else {
-        visualSlider.destroy(true, true);
         bestList.destroy(true, true);
         newList.destroy(true, true);
     }
@@ -85,6 +83,11 @@ const reviewList = new Swiper(".review-list", {
     navigation: {
         nextEl: ".review .btn-next",
         prevEl: ".review .btn-prev",
+    },
+
+    pagination: {
+        el: ".review .swiper-pagination",
+        type: "bullets",
     },
 
     breakpoints: {
